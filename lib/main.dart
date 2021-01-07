d8f9897fc403670dc75e5237aaa113c5125e3273
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2021-01-07 12:43:39 +0800

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,75 @@
-import 'package:flutter/material.dart';class MyAppBar extends StatelessWidget {  MyAppBar({this.title});  // Fields in a Widget subclass are always marked "final".  final Widget title;  @override  Widget build(BuildContext context) {    return Container(      height: 56.0, // in logical pixels      padding: const EdgeInsets.symmetric(horizontal: 8.0),      decoration: BoxDecoration(color: Colors.blue[800]),      // Row is a horizontal, linear layout.      child: Row(        // <Widget> is the type of items in the list.        children: <Widget>[          IconButton(            icon: Icon(Icons.arrow_back),            tooltip: 'Back',            disabledColor: Colors.white,            onPressed: null, // null disables the button          ),          // Expanded expands its child to fill the available space.          Expanded(            child: title,          ),          IconButton(            icon: Icon(Icons.cast),            tooltip: 'cast',            onPressed: null,            disabledColor: Colors.white,          ),          IconButton(            icon: Icon(Icons.search),            tooltip: 'Search',            onPressed: null,            disabledColor: Colors.white,          ),          IconButton(            disabledColor: Colors.white,            icon: Icon(Icons.more_vert),            tooltip: 'more',            onPressed: null,          ),        ],      ),    );  }}class MyScaffold extends StatelessWidget {  @override  Widget build(BuildContext context) {    // Material is a conceptual piece of paper on which the UI appears.    return Material(      // Column is a vertical, linear layout.      child: Column(        children: <Widget>[          MyAppBar(            title: Text(              'Tech Talks With AK',              style: Theme.of(context).primaryTextTheme.headline6,            ),          ),          Expanded(            child: Center(              child: Text('Hello, world!'),            ),          ),        ],      ),    );  }}void main() {  runApp(MaterialApp(    title: 'My app', // used by the OS task switcher    home: SafeArea(      child: MyScaffold(),    ),  ));}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Tech Talks",
+    home: SafeArea(
+      child: MyScaffold(),
+    ),
+  ));
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text("Tech Talks With AK",
+                style: Theme.of(context).primaryTextTheme.headline6),
+          ),
+          Expanded(
+              child: Center(
+            child: Text(
+                "Subscribe this channel to get more interesting technical videos"),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[800]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: null,
+            tooltip: "back",
+            disabledColor: Colors.white,
+          ),
+          Expanded(child: title),
+          IconButton(
+            icon: Icon(Icons.cast),
+            onPressed: null,
+            tooltip: "back",
+            disabledColor: Colors.white,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+            tooltip: "back",
+            disabledColor: Colors.white,
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: null,
+            tooltip: "back",
+            disabledColor: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
