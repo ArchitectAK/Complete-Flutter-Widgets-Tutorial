6fd590a2e88d4a76c2b7eeca3a65c4c1cbb5e160
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2021-01-23 23:34:45 +0800

    container constraints width

--- lib/5-container/ContainerDemo.dart
+++ lib/5-container/ContainerDemo.dart
@@ -27,12 +27,12 @@ class ContainerDemo extends StatelessWidget {
           child: Container(
               color: Colors.green,
               padding: EdgeInsets.all(8.0),
-              constraints: BoxConstraints.expand(),
+              // constraints: BoxConstraints.expand(),
+              constraints: BoxConstraints(minWidth: 100, maxWidth: 200),
               child: Text(
                 "Welcome to Flutter Tutorial",
                 style: TextStyle(fontSize: 20, color: Colors.white),
-              )
-          ),
+              )),
         ));
   }
 }
